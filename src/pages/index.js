1655f6a93f11b4d934fea62fc9c91f0d81ba5f28
willy <willypratama231@gmail.com>
2023-09-30 19:35:28 +0700

    Add Image from next and img

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,8 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <img src="/assets/img2.jpg" alt="gambar" className="h-[300px] w-auto" />
+      <Image src="/assets/img1.jpg" width={700} height={300} alt="gambar" />
       <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
         <p className="fixed top-0 left-0 flex justify-center w-full pt-8 pb-6 border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editings&nbsp;
